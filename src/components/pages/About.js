a3b09d971f6f75e53f46e310b0b28566576196e1
srishab <iamrishab19@gmail.com>
2019-10-19 01:52:29 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a TodoList App v1.0.0. It is made by using React</p>
+    </React.Fragment>
+  );
+}
+
+export default About;
